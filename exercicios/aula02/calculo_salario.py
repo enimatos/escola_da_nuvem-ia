67ab5931762f487a1aee6b30dbbc80adb854fee9
Elaine Matos <69444237+enimatos@users.noreply.github.com>
2024-11-22 01:06:39 -0300

    Delete exercicios/aula02 directory

--- exercicios/aula02/calculo_salario.py
+++
@@ -1,8 +0,0 @@
-matricula = int(input("Digite a matricula do colaborador: "))
-horas_trabalhadas = int(input("Digite quantas horas trabalhadas o colaborador teve: "))
-valor_hora = float(input("Digite o valor que o colaborador recebe por hora: "))
-
-salario = horas_trabalhadas * valor_hora
-
-print(f'NUMBER = {matricula}')
-print(f"SALARY = U$ {salario:.2f}")
--- exercicios/aula02/numero_par_ou_div_por_tres.py
+++
@@ -1,5 +0,0 @@
-# enquanto o número (index) estiver
-
-for i in range(1,101):
-    if i % 10 == 0:
-        print(f"O número {i} é divisível por 10")
